42ce0ab8d94005f783e9182b0f5bfbc15060cfba
slbotbm <rimukshkansal@gmail.com>
2023-10-09 14:23:55 +0900

    Recommiting everything

---
+++ priceCompare/app/Http/Controllers/SupermarketController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supermarket;
+use Illuminate\Http\Request;
+
+class SupermarketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Supermarket $supermarket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Supermarket $supermarket)
+    {
+        //
+    }
+}

094d5036e2c2f8f2330a3b971efb45646e1bfedd
slbotbm <rimukshkansal@gmail.com>
2023-10-11 05:12:08 +0900

    created ingredient factory

--- priceCompare/database/factories/IngredientFactory.php
+++ priceCompare/database/factories/IngredientFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Ingredient;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,13 @@ class IngredientFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Ingredient::class;
+    
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+
         ];
     }
 }

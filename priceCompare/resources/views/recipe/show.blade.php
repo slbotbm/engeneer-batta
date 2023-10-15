dfa0fdb27b27c7a7f7f43c17776e10579fe13442
slbotbm <rimukshkansal@gmail.com>
2023-10-15 13:10:04 +0900

    価格比較の半分ぐらい作りました

--- priceCompare/resources/views/recipe/show.blade.php
+++ priceCompare/resources/views/recipe/show.blade.php
@@ -21,19 +21,29 @@
 
             <div class="flex flex-col mb-4">
               <p class="mb-2 uppercase font-bold text-lg text-gray-800 dark:text-gray-200">{{$ingredients->count()}} Ingredients</p>
-              @foreach($ingredients as $ingredient)
-              <p class="py-2 px-3 text-gray-800 dark:text-gray-200" id="ingredients{{$loop->index}}">
-                {{$ingredient->name}}
-              </p>
-              @endforeach
-            </div>
+               <form action="{{route('recipe.price', $recipe->id)}}" method="POST"> 
+                @csrf
 
+              @foreach($ingredients as $ingredient)
+                
+                <input type="checkbox" id="{{$ingredient->name}}" name="ingredients[]" value="{{$ingredient}}" checked />
+                <label for="{{$ingredient->name}}" class="py-2 px-3 text-gray-800 dark:text-gray-200">{{$ingredient->name}}</p>
+            @endforeach
+            
+            <div class="flex items-left justify-end mt-4">
+                      <x-primary-button class="ml-3">
+                        Calculate cost
+                      </x-primary-button>
+</div>
             <div class="flex items-center justify-end mt-4">
               <a href="{{ url()->previous() }}">
                 <x-secondary-button class="ml-3">
                   {{ __('Back') }}
                 </x-primary-button>
               </a>
+            </form> 
+            
+            
             </div>
           </div>
         </div>
